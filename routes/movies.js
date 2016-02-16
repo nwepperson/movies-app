2077bd830c950373cb3c7cd1bf75d308f91c5daf
Nathan Epperson <nepperson25@gmail.com>
2016-02-16 14:05:15 -0500

    fucked up

--- routes/movies.js
+++ routes/movies.js
@@ -45,53 +45,77 @@ router.get('/new', authenticate, function(req, res, next) {
 router.post('/search', authenticate, function(req, res, next) {
   var currentUser = req.user;
   var filter = req.body.search;
+  var results = [];
   omdb.search(filter, function(err, movies) {
     if(err) {
         return console.error(err);
     }
-
     if(movies.length < 1) {
         return console.log('No movies were found!');
     }
-  res.render('movies/search', { movies: movies, message: req.flash() } );
-});
+    movies.forEach(function(item) {
+      omdb.get({ title: item.title }, true, function(err, film) {
+        if(err) {
+          return console.error(err);
+        }
+        if(!film) {
+          return console.log('Movie not found!');
+        }
+        var movie = {
+          title: film.title,
+          release_year: film.year,
+          description: film.plot,
+          photo_url: film.poster
+        };
+        results.push(movie);
+        console.log(results);
+      });
+    });
+    res.render('movies/search', { movies: results, message: req.flash() } );
+  });
 });
 
 //ADD
 router.post('/add', authenticate, function(req, res, next) {
-  if (req.body.add.length > 1) {
-    var array = req.body.add;
-  }
-  else {
-    var array = [req.body.add];
-  };
-  console.log(array);
+  console.log("add");
+  console.log(req.body.add);
   var currentUser = req.user;
-  for (i = 0; i < array.length; i++) {
-    omdb.get({ title: array[i] }, true, function(err, film) {
-      if(err) {
-          return console.error(err);
-      }
-
-      if(!film) {
-          return console.log('Movie not found!');
-      }
-      var movie = {
+  omdb.get({ title: req.body.add }, true, function(err, film) {
+    if(err) {
+      return console.error(err);
+    }
+    if(!film) {
+      return console.log('Movie not found!');
+    }
+    var movie = {
       title: film.title,
       release_year: film.year,
       description: film.plot,
       photo_url: film.poster
-      };
-  currentUser.movies.push(movie);
-  currentUser.save()
-  .then(function() {
-  }, function(err) {
-    return next(err);
+    };
+    var matchstat = false;
+    array = currentUser.movies;
+    array.forEach(function(match) {
+      if (match.title == movie.title) {
+        if (match.release_year == movie.release_year) {
+          if (match.title == movie.title) {
+            if (match.title == movie.title) {
+              matchstat = true;
+            }
+          }
+        }
+      }
+    });
+    if (matchstat == false) {
+      currentUser.movies.push(movie);
+    };
+    currentUser.save()
+    .then(function() {
+      res.redirect('/movies');
+    }, function(err) {
+      return next(err);
+    });
   });
-  });
-  };
-  console.log(global.currentUser.movies);
-  res.redirect('/movies');
 });
 
 //SHOW
@@ -106,6 +130,14 @@ router.get('/:id', authenticate, function(req, res, next) {
   // }, function(err) {
   //   return next(err);
   // });
+});
+
+//SHOW RESULT
+router.get('/:id/result', authenticate, function(req, res, next) {
+  var currentUser = req.user;
+  var movie = currentUser.results.id(req.params.id);
+  if (!movie) return next(makeError(res, 'Document not found', 404));
+  res.render('movies/showresult', { movie: movie, message: req.flash() } );
 });
 
 // CREATE
